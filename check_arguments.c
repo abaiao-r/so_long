bcb65c872d94af2371f04bf34aaaa4291998dda0
Andre Francisco Baiao Rolao Candido Da Silva <abaiao-r@c1r12s2.42lisboa.com>
2023-04-10 19:46:04 +0100

    fix check_arguments_input by adding a condition to check if file is .ber

--- check_arguments.c
+++ check_arguments.c
@@ -6,11 +6,25 @@
 /*   By: abaiao-r <abaiao-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:34:34 by abaiao-r          #+#    #+#             */
-/*   Updated: 2023/04/10 15:18:54 by abaiao-r         ###   ########.fr       */
+/*   Updated: 2023/04/10 19:44:44 by abaiao-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	check_map_name(char *str)
+{
+	size_t	i;
+
+	i = ft_strlen(str) - 1;
+	if (str[i] != 'r' || str[i - 1] != 'e' || str[i - 2] != 'b' || str[i
+			- 3] != '.')
+		return (1);
+	if (str[0] != 'm' || str[1] != 'a' || str[2] != 'p' || str[3] != 's'
+		|| str[4] != '/')
+		return (1);
+	return (0);
+}
 
 /* Funtion returns a 0 if the terminal input to run the program is wrong.
 It checks if the number of arguments is 2.
@@ -24,6 +38,11 @@ int	check_arguments_input(int ac, char **av)
 	{
 		ft_printf("Your input on terminal should \
 be: ./so_long maps/<map_name>.ber");
+		return (0);
+	}
+	else if (ac == 2 && check_map_name(av[1]))
+	{
+		ft_printf("Wrong map name. Map name should be maps/<map_name>.ber");
 		return (0);
 	}
 	else
